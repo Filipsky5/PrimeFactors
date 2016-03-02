19064aa1fcef946ff2dba7412e973441071601a6
Filip Jabłoński <filip.g.jablonski@gmail.com>
2016-03-02 21:52:31 +0100

    initial commit

---
+++ PrimeFactors/PrimeFactors.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrimeFactors
+{
+    public class PrimeFactors
+    {
+        public static List<int> ComputeFactors(int i)
+        {
+            List<int> primeFactors = new List<int>();
+            
+            for(int candidate = 2;i> 1;candidate++) { 
+                for(;i%candidate == 0; i /= candidate) { 
+                    primeFactors.Add(candidate);
+                }
+                
+            }
+            return primeFactors;
+        }
+    }
+}
